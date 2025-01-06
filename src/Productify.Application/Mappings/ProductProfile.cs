8c40374abef7430bcfc174ce18aff38b1c06ce0f
Lucas Silva <lucas.sva@outlook.com>
2025-01-06 16:39:52 -0300

    fix: corrected non-nullable warnings and updated CI runner version

--- src/Productify.Application/Mappings/ProductProfile.cs
+++ src/Productify.Application/Mappings/ProductProfile.cs
@@ -9,8 +9,7 @@ public class ProductProfile : Profile
     public ProductProfile()
     {
         CreateMap<Product, ProductDto>()
-            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
-
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category != null ? src.Category.Name : string.Empty));
         CreateMap<CreateProductDto, Product>();
         CreateMap<UpdateProductDto, Product>();
     }
